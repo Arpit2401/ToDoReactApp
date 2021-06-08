f6f4d7c602145f239e459a1dad23a9182a60f2a7
Arpit Agarwal <arpitmanas28@gmail.com>
2021-06-08 23:15:21 +0530

    Added Simple ToDo React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, { useState} from 'react'
 import './App.css';
 
+
+
 function App() {
+
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  const InputData = (e) => setInput(e.target.value);
+
+  const AddToDo = (event) =>{
+    event.preventDefault();
+    setItems( (prevItems) =>{return [...prevItems, input]});
+    setInput("");
+  };
+
+  const ToDoDelete = (id) => {
+    setItems((prevItems) =>{return prevItems.filter((element,index) => index!==id)});
+  
+  };
+
+  const Heading = () =>{
+    return(
+        <div className="todo-heading">
+        <img src="icontodo.png" alt="Logo"/>
+        <h1>ToDoistic : Create Your Own ToDo's!</h1>
+        </div>
+    );
+  };
+   const ToDoCard = (props) =>{
+    return(
+        <div className="todo-card">
+            <p>{props.todo} 
+            </p>
+            <button onClick={() => props.onDelete(props.id)}><i className="fas fa-times"></i></button>
+        </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className="todo-container">
+          <Heading/>
+          <div className="todo-create">
+          <form>
+            <input type="text" required placeholder="Add Your ToDo..." value={input} onChange={InputData}/>
+            <button type="submit" onClick={AddToDo}>+</button>
+          </form>
+        </div>
+          <div className="todo-list">
+          {items.map( (itemValue, index) => {
+            return <ToDoCard key={index} id={index} todo={itemValue} onDelete={ToDoDelete}/>})}      
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
